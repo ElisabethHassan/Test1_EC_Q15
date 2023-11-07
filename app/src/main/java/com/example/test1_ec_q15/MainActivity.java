cca14d11ed5b25b9a0b3634ae6e3f5f896fd8b34
elisabethhassan <elisabethh.info@gmail.com>
2023-11-07 10:10:49 -0600

    update functionalities

--- app/src/main/java/com/example/test1_ec_q15/MainActivity.java
+++ app/src/main/java/com/example/test1_ec_q15/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.test1_ec_q15;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.View;
@@ -12,19 +16,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView phoneNum;
     String number;
-    MyReceiver receiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         number = getIntent().getStringExtra("number");
         phoneNum = findViewById(R.id.numberTV_id);
-        receiver = new MyReceiver();
 
-        IntentFilter filter = new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
-        registerReceiver(receiver, filter);
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+//                != PackageManager.PERMISSION_GRANTED){
+//            String[] perms = new String[]{android.Manifest.permission.READ_PHONE_STATE};
+//            ActivityCompat.requestPermissions(this,perms, 101);
+//        }
 
-        phoneNum.setText(number);
+        if (phoneNum != null) {
+            phoneNum.setText(number);
+        } else phoneNum.setText("No incoming calls at the moment");
 
     }
 
--- app/src/main/java/com/example/test1_ec_q15/MyReceiver.java
+++ app/src/main/java/com/example/test1_ec_q15/MyReceiver.java
@@ -10,11 +10,17 @@ public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
         if (state != null && state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
             String incomingNum = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            if (incomingNum != null){
+                Intent intent1 = new Intent(context, MainActivity.class);
+                intent1.putExtra("number", incomingNum);
+                intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP) ;
+                context.startActivity(intent1);
+            }
         }
     }
 }
